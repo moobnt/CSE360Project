e054d770a9eccc8cfc8acf755debb3f652da869b
caward10 <114179391+caward10@users.noreply.github.com>
2024-10-09 22:33:30 -0700

    Instructor-doc / student

--- src/project/Instructor.java
+++ src/project/Instructor.java
@@ -9,12 +9,30 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
+
+/**
+ * 
+ * <p> Instructor - Web page </p>
+ * 
+ * <p> Description: This page is set up for the Instructor, it allows them to view, edit, and post topics
+ *  for the student to view. The code is based on a TilePlan for inputs based of of user preference. </p>
+ * 
+ * 
+ * 
+ * @author Group TH 58
+ * 
+ * @version 1.00		2024-10-9	Initial baseline
+ * 
+ */
+
+
+
 public class Instructor extends TilePane {
 	
 	
 		//as it pops up it starts with button
     public Instructor(Stage stage, User user, DatabaseModel database) {
-    	
+    	//title page
         stage.setTitle("Instructor Home page");
         
         setHgap(10); // Horizontal gap between tiles
@@ -25,7 +43,8 @@ public class Instructor extends TilePane {
         btn.setOnAction(new EventHandler<>() {
         	
         	public void handle(ActionEvent event) {
-        
+        		
+        		//send back to login page
         		LoginService login = new LoginService(stage, user, database);
 
                    
--- src/project/Student.java
+++ src/project/Student.java
@@ -6,46 +6,36 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
-public class Student extends Application {
+public class Student extends TilePane {
 	
-	//launches button
-	public static void main(String[] args) {
-        launch(args);
-    }
-    
+	
 	//as it pops up it starts with button
-    public void start(Stage primaryStage) {
-    	
-        primaryStage.setTitle("Student Home page");
-        Button btn = new Button();
-        btn.setText("Log Out");
-        btn.setOnAction(new EventHandler<>() {
-            public void handle(ActionEvent event) {
-            	
-	            		/***
-	            		 * this event occures when the log out button is played. When the button is pressed
-	            		 * it results in the application closing the current window
-	            		 * and running the new window I.e the log out page
-	            		 */
-	            		
-//	            		  // Start the external Java process
-//		                   LoginService.main(new String[0]);
-//		                    
-//		                    //*****
-//		                    
-//		                    	// Get the current window
-//		                        Stage stage = (Stage) btn.getScene().getWindow(); 
-//		                        // Close the current window
-//		                        stage.close(); 
-		                   
-            }
-        });
+    public Student(Stage stage, User user, DatabaseModel database) {
+    	//title page
+        stage.setTitle("Student Home page");
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+        setHgap(10); // Horizontal gap between tiles
+        setVgap(10); 
+        
+        Button btn = new Button("log out");
+        
+        btn.setOnAction(new EventHandler<>() {
+        	
+        	public void handle(ActionEvent event) {
+        		//send back to login page
+        		LoginService login = new LoginService(stage, user, database);
+
+                   
+                } 
+            });
+            
+        
+        getChildren().add(btn);
+        stage.setScene(new Scene(this, 300, 250));
+        stage.show();
     }
+}
 }
