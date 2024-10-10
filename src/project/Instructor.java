cac75b893f6b9e886d0f966bfed470c318ab0bb6
caward10 <114179391+caward10@users.noreply.github.com>
2024-10-09 22:13:22 -0700

    Instructor

--- src/project/Instructor.java
+++ src/project/Instructor.java
@@ -8,25 +8,22 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Instructor {
+public class Instructor extends TilePane{
 	
-	public static void main(String[] args) {
-		System.out.println("helo");
-	}
-//	//launches button
-//		public static void main(String[] args) {
-//	        launch(args);
-//	    }
-//	    
+	
 		//as it pops up it starts with button
     public void start(Stage primaryStage) {
     	
         primaryStage.setTitle("Instructor Home page");
-        Button btn = new Button();
-        btn.setText("Log Out");
-        btn.setOnAction(new EventHandler<>() {
+        
+        TilePane tilePane = new TilePane();
+        tilePane.setHgap(10); // Horizontal gap between tiles
+        tilePane.setVgap(10); 
+        Button btn = new Button("log out");
+        
+        
             public void handle(ActionEvent event) {
-            	
+            	@Override
 	            		/***
 	            		 * this event occures when the log out button is played. When the button is pressed
 	            		 * it results in the application closing the current window
@@ -34,21 +31,21 @@ public class Instructor {
 	            		 */
 	            		
 	                    // Start the external Java process
-//	                   LoginService.main(new String[0]);
-//	                    
-//	                    //*****
-//	                    
-//	                    	// Get the current window
-//	                        Stage stage = (Stage) btn.getScene().getWindow(); 
-//	                        // Close the current window
-//	                        stage.close(); 
-//	                   
+	                   
+	                    
+	                    //*****
+	                    
+	                    	// Get the current window
+	                         
+	                        // Close the current window
+	                       
+	                   
 	                } 
 	            });
             
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        tilePane.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
