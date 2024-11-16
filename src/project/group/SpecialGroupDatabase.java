481183356da599716e226500fc7ba5dfb5217ab2
moobnt <mars.dawn.carnes@gmail.com>
2024-11-16 11:12:35 -0700

    Implemented addPerson and removePerson #17

--- src/project/group/SpecialGroupDatabase.java
+++ src/project/group/SpecialGroupDatabase.java
@@ -132,33 +132,33 @@ public class SpecialGroupDatabase extends DatabaseModel {
     }
 
     /**
-     * Adds a student to a specific group
+     * Adds a person to a special group
      * 
-     * @param title Unique title of the group
-     * @param student Student to add to the group
+     * @param title The title of the group
+     * @param role The role of the person to be added
+     * @param person The username of the person to be added
      */
-    public void addStudent(String title, String student) {
-        String studentListString = null;
-        // initial query to grab the list of students in a group
-        String getStudent = "SELECT students FROM ? WHERE title = ?";
+    public void addPerson(String title, String role, String person) {
+        String roleListString = null;
+        String getPeople = "SELECT ? FROM ? WHERE title = ?";
 
-        try (PreparedStatement pstmt = connection.prepareStatement(getStudent)) {
-            pstmt.setString(1, TABLE_NAME);
-            pstmt.setString(2, title);
+        try (PreparedStatement pstmt = connection.prepareStatement(getPeople)) {
+            pstmt.setString(1, role);
+            pstmt.setString(2, TABLE_NAME);
+            pstmt.setString(3, title);
 
-            ResultSet studentResultSet = pstmt.executeQuery();
+            ResultSet resultSet = pstmt.executeQuery();
 
-            // adds student to the current string of students
-            studentListString = addItemToString(studentResultSet.getString("students"), student);
+            roleListString = addItemToString(resultSet.getString(role), person);
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
 
-        String addStudent = "UPDATE ? SET students = ? WHERE title = ?";
+        String addPerson = "UPDATE ? SET ? = ? WHERE title = ?";
 
-        try (PreparedStatement pstmt = connection.prepareStatement(addStudent)) {
+        try (PreparedStatement pstmt = connection.prepareStatement(addPerson)) {
             pstmt.setString(1, TABLE_NAME);
-            pstmt.setString(2, studentListString);
+            pstmt.setString(2, roleListString);
             pstmt.setString(3, title);
 
             pstmt.executeUpdate();
@@ -168,22 +168,24 @@ public class SpecialGroupDatabase extends DatabaseModel {
     }
 
     /**
-     * Removes a student from a specific group
+     * Removes a person from a specific group
      * 
      * @param title Unique title of the group
-     * @param student Student to remove from the group
+     * @param role The role of the person to be removed
+     * @param person Person to remove from the group
      */
-    public void removeStudent(String title, String student) {
-        String studentListString = null;
-        String getStudents = "SELECT students FROM ? WHERE title = ?";
+    public void removePerson(String title, String role, String person) {
+        String roleListString = null;
+        String getPeople = "SELECT ? FROM ? WHERE title = ?";
 
-        try (PreparedStatement pstmt = connection.prepareStatement(getStudents)) {
-            pstmt.setString(1, TABLE_NAME);
-            pstmt.setString(2, title);
+        try (PreparedStatement pstmt = connection.prepareStatement(getPeople)) {
+            pstmt.setString(1, role);
+            pstmt.setString(2, TABLE_NAME);
+            pstmt.setString(3, title);
 
             ResultSet studentResultSet = pstmt.executeQuery();
 
-            studentListString = removeItemFromString(studentResultSet.getString("students"), student);
+            roleListString = removeItemFromString(studentResultSet.getString("students"), person);
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
@@ -192,80 +194,10 @@ public class SpecialGroupDatabase extends DatabaseModel {
 
         try (PreparedStatement pstmt = connection.prepareStatement(removeStudent)) {
             pstmt.setString(1, TABLE_NAME);
-            pstmt.setString(2, studentListString);
+            pstmt.setString(2, roleListString);
             pstmt.setString(3, title); 
 
             pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    /**
-     * Adds an admin to a group
-     * When calling this function, it should be ensured that only a current
-     * admin of the group can make other admins
-     * 
-     * @param title Unique title of the group
-     * @param admin Admin to add to the group
-     */
-    public void addAdmin(String title, String admin) {
-        String addAdmin = "UPDATE ? SET admins = ? WHERE title = ?"; // TODO: fix it
-
-        try (PreparedStatement pstmt = connection.prepareStatement(addAdmin)) {
-
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    /**
-     * Removes an admin from a group
-     * Implements a check to ensure that, by removing someone as an admin, a group
-     * will not be without admins
-     * 
-     * @param title Unique title of the group
-     * @param admin Admin to remove from the group
-     */
-    public void removeAdmin(String title, String admin) {
-        String removeAdmin = ""; // TODO: fix it
-
-        try (PreparedStatement pstmt = connection.prepareStatement(removeAdmin)) {
-
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    /**
-     * Adds an article to a group
-     * This should make it so that anyone within the group
-     * can decrypt the data within the articles
-     * 
-     * @param title Unique title of the group
-     * @param articleID The ID of the article to add to the group
-     */
-    public void addArticle(String title, int articleID) {
-        String addArticle = ""; // TODO: fix it
-
-        try (PreparedStatement pstmt = connection.prepareStatement(addArticle)) {
-
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    /**
-     * Removes an article from a group
-     * 
-     * @param title Unique title of the group
-     * @param articleID The ID of the article to remove from the group
-     */
-    public void removeArticle(String title, int articleID) {
-        String removeArticle = ""; // TODO: fix it
-
-        try (PreparedStatement pstmt = connection.prepareStatement(removeArticle)) {
-
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
