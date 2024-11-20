c7c28275429c03592a08312ef7de8060c2d1d129
moobnt <mars.dawn.carnes@gmail.com>
2024-11-20 15:29:23 -0700

    Created Search class

---
+++ src/project/search/Search.java
@@ -0,0 +1,16 @@
+package project.search;
+
+/**
+ * <p> Search Class </p>
+ * 
+ * <p> Description: The class that outlines methods for searching. 
+ *                  People can search using: words, names, or phrases in the title, author, or abstract. 
+ *                  A search can also be performed with the long identifier for an article. </p>
+ * 
+ * @author Group TH 58
+ * 
+ * @version 1.00    2024-11-20  Initial
+ */
+public class Search {
+    
+}
